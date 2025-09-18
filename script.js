32db83cec5a181c5a03583a50dd6071e00f37f83
JimbobWarlord <s4092060@student.rmit.edu.au>
2025-09-18 15:50:20 +1000

    presentation

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("keydown", function (event) {
+  if (event.key.toLowerCase() === "r") {
+    document.body.style.background = "radial-gradient(#782623, black)";
+  }
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.key.toLowerCase() === "b") {
+    document.body.style.background = "radial-gradient(#253a5c, black)";
+  }
+});
